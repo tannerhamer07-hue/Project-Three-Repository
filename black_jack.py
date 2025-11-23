30c35a8b5d4419ac1342920587fa9280045d4142
Aiden Stainbrook <astainbrook@mocs.flsouthern.edu>
2025-11-22 22:47:27 -0500

    Added comments relating to leaderbaord

--- black_jack.py
+++ black_jack.py
@@ -2,9 +2,13 @@
 
 def main():
     # print("Hello, World!")
+    #Would want something like update_leaderboard(player_name, did_win)
+    #show_leaderboard() as well at end of the game
     pass
+    
 
 def welcome_and_sign_in():
+    #Would want to get and return player name using what I used in leaderboard.py
     pass
 
 def deal_card():
@@ -20,6 +24,7 @@ def ask_to_hit_or_stand_and_loop():
     pass
 
 def win_and_lose_conditions():
+    #Return win or lose status
     pass
 
 if __name__ == "__main__":
